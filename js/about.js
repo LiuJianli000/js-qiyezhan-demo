a4e87af256f8d13a999a53adc020a97e962022c2
liujianli <376122478@qq.com>
2020-06-17 11:43:00 +0800

    first modify

--- js/about.js
+++ js/about.js
@@ -3,7 +3,7 @@
 
 // main
 axios.get('./router/about.json')
-    .then(function(res) {
+    .then(function (res) {
         $('main > ul').append(`
             <li class="map-show" id="allmap"></li>
         `)
@@ -12,11 +12,11 @@ axios.get('./router/about.json')
         var point = new BMap.Point(13274766.191805037, 2987611.95223);
         var marker = new BMap.Marker(point);  // 创建标注
         map.addOverlay(marker);              // 将标注添加到地图中
-        map.centerAndZoom(point, 15);
+        map.centerAndZoom(point, 1);
         map.enableScrollWheelZoom(true);
 
-        
-        res.data.about.forEach(function(item) {
+
+        res.data.about.forEach(function (item) {
             $('main > ul .aboutText').append(`
                 <p>
                     <span class="iconfont ${item.icon}"></span>
@@ -30,20 +30,20 @@ axios.get('./router/about.json')
 
 // footer
 axios.get('./router/about.json')
-            .then(function(res) {
-                console.log(res)
-                res.data.about.forEach(function(item) {
-                    $('footer > ul').append(`
-                        <li>
-                            <a href="./about.html"><span class="iconfont ${item.icon}"></span></a><br>
-                            <span>${item.text}</span>
-                        </li> 
-                    `)
-                })
-                $('footer > ul').append(`
-                    <li>
-                    <a href="./about.html"><span><img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1838781477,1688363439&fm=115&gp=0.jpg" alt="" /></span></a><br/>
-                        <span>扫一扫 加入复仇者联盟</span>
-                    </li> 
-                `)
-            })
+    .then(function (res) {
+        console.log(res)
+        res.data.about.forEach(function (item) {
+            $('footer > ul').append(`
+                <li>
+                    <a href="./about.html"><span class="iconfont ${item.icon}"></span></a><br>
+                    <span>${item.text}</span>
+                </li> 
+            `)
+        })
+        $('footer > ul').append(`
+            <li>
+                <a href="./about.html"><span><img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1838781477,1688363439&fm=115&gp=0.jpg" alt="" /></span></a><br/>
+                <span>扫一扫 加入复仇者联盟</span>
+            </li> 
+        `)
+    })
--- js/index.js
+++ js/index.js
@@ -3,120 +3,66 @@ $(document).ready(function () {
     axios.get('./router/banner-img.json')
         .then(function (res) {
             res.data.forEach(function (item) {
-                $('#banner-img').append(`<li><img src="${item.src}" /></li>`)
+                $('.panels').append(`<a href="#"><img src="${item.src}"></a>`)
             });
 
+            class Carousel {
+                constructor(root) {
+                    this.root = root
+                    this.panels = Array.from(root.querySelectorAll('.panels a'))
+                    this.dotCt = root.querySelector('.dots')
+                    this.dots = Array.from(root.querySelectorAll('.dots span'))
+                    this.pre = root.querySelector('.pre')
+                    this.next = root.querySelector('.next')
 
-            function Carousel($ct) {
-                this.init($ct)
-                this.bind()
-                this.autoPlay()
-            }
-            Carousel.prototype = {
-                init: function ($ct) {
-                    this.$ct = $ct
-                    this.$bannerImg = this.$ct.find('#banner-img')
-                    this.$imgs = this.$ct.find('#banner-img > li')
-                    this.$preBtn = this.$ct.find('.pre')
-                    this.$nextBtn = this.$ct.find('.next')
-                    this.$bullets = this.$ct.find('.bullets li')
-
-                    this.imgCount = this.$imgs.length
-                    this.imgWidth = this.$imgs.width()
-                    this.index = 0
-                    this.isAnimate = false
-
-                    this.$bannerImg.append(this.$imgs.first().clone())
-                    this.$bannerImg.prepend(this.$imgs.last().clone())
-
-                    this.$bannerImg.width((this.imgCount + 2) * this.imgWidth)
-                    this.$bannerImg.css('left', - this.imgWidth)
-
-                },
-                bind: function () {
-                    var _this = this
-                    this.$nextBtn.on('click', function () {
-                        // console.log('next...')
-                        _this.playNext(1)
-                    })
-                    this.$preBtn.on('click', function () {
-                        // console.log('pre...')
-                        _this.playPre(1)
-                    })
-                    this.$bullets.on('click', function () {
-                        // console.log('index: ' + $(this).index())
-                        var index = $(this).index()
-                        if (_this.index > index) {
-                            _this.playPre(_this.index - index)
-                        } else {
-                            _this.playNext(index - _this.index)
-                        }
-                    })
-                },
-                playNext: function (len) {
-                    var _this = this
-                    if (this.isAnimate) return
-                    this.isAnimate = true
-                    this.$bannerImg.animate({
-                        left: '-=' + this.imgWidth * len
-                    }, function () {
-                        _this.index += len
-                        if (_this.index === _this.imgCount) {
-                            _this.$bannerImg.css('left', -_this.imgWidth)
-                            _this.index = 0
-                        }
-                        // console.log('_this.index: ' + _this.index)
-                        _this.setBullet()
-                        _this.isAnimate = false
-                    })
-
-                },
-                playPre: function (len) {
-                    var _this = this
-                    if (_this.isAnimate) return
-                    _this.isAnimate = true
-                    this.$bannerImg.animate({
-                        left: '+=' + this.imgWidth * len
-                    }, function () {
-                        _this.index -= len
-                        if (_this.index < 0) {
-                            _this.$bannerImg.css('left', -_this.imgWidth * _this.imgCount)
-                            _this.index = _this.imgCount - 1
-                        }
-                        // console.log('_this.index: ' + _this.index)
-                        _this.setBullet()
-                        _this.isAnimate = false
-                    })
-                },
-
-                setBullet: function () {
-                    this.$bullets.eq(this.index).addClass('active')
-                        .siblings().removeClass('active')
-                },
-                autoPlay: function () {
-                    var _this = this
-                    this.autoClock = setInterval(function () {
-                        _this.playNext(1)
-                    }, 3000)
-                },
-                stopAuto: function () {
-                    clearInterval(this.autoClock)
+                    this.bind()
                 }
 
+                bind() {
+                    this.dotCt.onclick = e => {
+                        if (e.target.tagName !== 'SPAN') return
 
+                        let index = this.dots.indexOf(e.target)
+                        this.setDot(index)
+                        this.showPage(index)
+                    }
+
+                    this.pre.onclick = e => {
+                        let index = this.dots.indexOf(this.root.querySelector('.dots .active'))
+                        index = (index - 1 + this.dots.length) % this.dots.length
+                        this.setDot(index)
+                        this.showPage(index)
+                    }
+
+                    this.next.onclick = e => {
+                        let index = this.dots.indexOf(this.root.querySelector('.dots .active'))
+                        index = (index + 1) % this.dots.length
+                        this.setDot(index)
+                        this.showPage(index)
+                    }
+                }
+
+                setDot(index) {
+                    this.dots.forEach(dot => dot.classList.remove('active'))
+                    this.dots[index].classList.add('active')
+                }
+
+                showPage(index) {
+                    this.panels.forEach(panel => panel.style.zIndex = 0)
+                    this.panels[index].style.zIndex = 1
+                }
             }
 
-            new Carousel($('#banner'))
-
+            new Carousel(document.querySelector('.carousel'))
         })
 
-        // 获取团队信息
-        axios.get('./router/team-info.json')
-            .then(function(res) {
-                // console.log(res)
-                res.data.team.forEach(function(item){
-                    // console.log(item.img)
-                    $('#team > ul').append(`
+    // 获取团队信息
+    axios.get('./router/team-info.json')
+        .then(function (res) {
+            // console.log(res)
+            res.data.team.forEach(function (item) {
+                // console.log(item.img)
+                $('#team > ul').append(`
                         <li>
                             <div class="contentImg"><img src="${item.img}" alt=""></div>
                             <div class="contentBox">
@@ -127,15 +73,15 @@ $(document).ready(function () {
                             </div>
                         </li>
                     `)
-                })
             })
+        })
 
-        // 获取新闻信息
-        axios.get('./router/news.json')
-            .then(function(res) {
-                // console.log(res)
-                res.data.news.forEach(function(item) {
-                    $('#news > ul').append(`
+    // 获取新闻信息
+    axios.get('./router/news.json')
+        .then(function (res) {
+            // console.log(res)
+            res.data.news.forEach(function (item) {
+                $('#news > ul').append(`
                         <li>
                             <a href="./news.html"><img src="${item.img}" alt=""></a>
                             <p class="newsTitle">${item.title}</p>
@@ -143,27 +89,27 @@ $(document).ready(function () {
                             <p class="newsText">${item.text}</p>
                         </li> 
                     `)
-                })
             })
+        })
 
-        // footer
-        axios.get('./router/about.json')
-            .then(function(res) {
-                console.log(res)
-                res.data.about.forEach(function(item) {
-                    $('footer > ul').append(`
+    // footer
+    axios.get('./router/about.json')
+        .then(function (res) {
+            console.log(res)
+            res.data.about.forEach(function (item) {
+                $('footer > ul').append(`
                         <li>
                             <a href="./about.html"><span class="iconfont ${item.icon}"></span></a><br>
                             <span>${item.text}</span>
                         </li> 
                     `)
-                })
-                $('footer > ul').append(`
+            })
+            $('footer > ul').append(`
                     <li>
                     <a href="./about.html"><span><img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1838781477,1688363439&fm=115&gp=0.jpg" alt="" /></span></a><br/>
                         <span>扫一扫 加入复仇者联盟</span>
                     </li> 
                 `)
-            })
-        
+        })
+
 })
