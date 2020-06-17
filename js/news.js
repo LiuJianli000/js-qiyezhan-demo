399825a366581677b245db9234feee7ffcf64c51
liujianli <376122478@qq.com>
2020-06-17 19:14:13 +0800

    pagination

--- js/news.js
+++ js/news.js
@@ -1,31 +1,30 @@
 // footer
-axios.get('./router/about.json')
-    .then(function (res) {
-        // console.log(res)
-        res.data.about.forEach(function (item) {
-            $('footer > ul').append(`
+$(document).ready(function () {
+    axios.get('./router/about.json')
+        .then(function (res) {
+            // console.log(res)
+            res.data.about.forEach(function (item) {
+                $('footer > ul').append(`
             <li>
                 <a href="./about.html"><span class="iconfont ${item.icon}"></span></a><br>
                 <span>${item.text}</span>
             </li> 
         `)
-        })
-        $('footer > ul').append(`
+            })
+            $('footer > ul').append(`
         <li>
         <a href="./about.html"><span><img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1838781477,1688363439&fm=115&gp=0.jpg" alt="" /></span></a><br/>
             <span>扫一扫 加入复仇者联盟</span>
         </li> 
     `)
-    })
+        })
 
-
-var num = 1
-function render() {
-    axios.get(`./router/newsinfo-page=${num}.json`)
-        .then(function (res) {
-            // console.log(res)
-            res.data.news.forEach(function (item) {
-                $('#newsInfo > ul').append(`
+    var total = 0
+    function fetch(num, refetch) {
+        axios.get(`http://mock-api.com/mgvrkAgQ.mock/newsinfo?page=${num}`)
+            .then(function (res) {
+                res.data.news.forEach(function (item) {
+                    $('#newsInfo > ul').append(`
                     <li>
                         <a href="./newslist.html" name="${item}"><img src="${item.img}" alt=""></a>
                         <div class="msg">
@@ -35,34 +34,58 @@ function render() {
                         </div>
                     </li>
                 `)
+                })
 
+                total = res.data.total
+                if (!refetch) {
+                    var page = Math.ceil(res.data.total / 8)
+                    $('#page ul').append(`<li id="pre"><</li>`)
+                    for (i = 1; i <= page; i++) {
+                        $('#page ul').append(`<li class="common">${i}</li>`)
+                    }
+                    $('#page ul').append(`<li id="next"">></li>`)
 
+                    $('#page ul .common:first').css({ "background-color": "#baeffd" })
+                }
             })
-        })
-        // .then(function(res) {
-        //     $('#newsInfo > ul > li').click(function() {
-        //         // let aa = $(this).find('a').attr('name')
-        //         console.log($(this).index())
-        //         let bb = $(this).index()
-        //         let aa = res.data.news[bb]
-        //         $('#titleDivAA').append(`
-        //             <span class="red"></span><p class="newsTitle">${aa.title}</p>
-        //             <p class="dateAA">${aa.date} &nbsp ${aa.date2}</p>
-        //         `)
-        //         $('#infoAA').append(`
-        //             <img src="${aa.img}" alt="">
-        //             <p>${aa.text}</p>
-        //             <p>${aa.text}</p>
-        //         `)
-        //     })
-        // })
-        
-}
-render()
+    }
 
-$('#page ul > li').click(function () {
-    $('#newsInfo > ul').empty()
-    num = $(this).index() + 1
+    const refetch = page => {
+        $('#newsInfo > ul').empty()
+        fetch(page, 'refetch')
+
+        $('#page ul').children('.common').each(function () {
+            if (page == $(this).text()) {
+                $('#page ul li').css({ "background": 'white' })
+                $(this).css({ "background-color": "#baeffd" })
+            }
+        })
+    }
+
+    const render = async () => {
+        var page = 1
+        await fetch(page)
+
+        $('#page ul').on('click', "#pre", function () {
+            if (page >= 2) {
+                page--
+                refetch(page)
+            }
+        })
+
+        $('#page ul').on('click', '.common', function () {
+            page = $(this).text()
+            refetch(page, $(this))
+        })
+
+        $('#page ul').on('click', "#next", function () {
+            if (page < total / 8) {
+                page++
+                refetch(page)
+            }
+        })
+    }
+
     render()
 })
 
